413c7ae93f02b0356ad91108171196915faf0ce8
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2026-06-15 09:48:50 -0500

    CU-06

--- SGSE_Springboot/src/main/java/com/example/SGSE/Controllers/ReporteController.java
+++ SGSE_Springboot/src/main/java/com/example/SGSE/Controllers/ReporteController.java
@@ -1,5 +1,31 @@
 package com.example.SGSE.Controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SGSE.Services.Reporte.QueryReporte;
+
+@RestController
+@RequestMapping("/Reporte/")
+@CrossOrigin(origins = "http://localhost:8080/")
 public class ReporteController {
+	
+	private final QueryReporte QReporte;
+
+	public ReporteController(QueryReporte qReporte) {
+		super();
+		QReporte = qReporte;
+	}
+	
+	@GetMapping("/Generar_Reporte")
+	public String generar_Reporte() {
+		
+		return QReporte.generar_Reporte();
+		
+	}
+	
+	
 
 }
--- SGSE_Springboot/src/main/java/com/example/SGSE/Services/Reporte/QueryReporte.java
+++ SGSE_Springboot/src/main/java/com/example/SGSE/Services/Reporte/QueryReporte.java
@@ -1,7 +1,10 @@
 package com.example.SGSE.Services.Reporte;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.SGSE.Models.Solicitud;
 import com.example.SGSE.Repositories.SolicitudRepository;
 
 @Service
@@ -14,8 +17,44 @@ public class QueryReporte {
 		SoliRep = soliRep;
 	}
 	
-	public void generar_Reporte() {
+	public String generar_Reporte() {
 		
+		String reporte ="";
+		
+		List<Solicitud> Solicitudes = SoliRep.findAll();
+		
+		if(Solicitudes.size() <= 0) {
+			
+			return "No se encontraron solicitude spara generar un reporte";
+		}
+		
+		reporte+="---------------------------------------- \n"
+				+ "Reporte de solicitudes \n"
+				+ "--------------------------------------- \n"
+				+ "Cantidad de solicitudes registradas = " + Solicitudes.size()+" \n"
+				+ "-------------------------------------- \n"
+				+ "Solicitudes registradas = \n";
+		
+		
+		
+		
+		for(Solicitud solicitud : Solicitudes) {
+			
+			
+			reporte+=" \n "
+					+"........................................................................................ \n "
+					+" ID Solicitud = " + solicitud.getIdSolicitud() + "\n"
+					+" Tipo de Solicitud = " + solicitud.getTipoSolicitud().getNombre() + "\n"
+					+" Tiempo de resolución = " + solicitud.getTipoSolicitud().getTiempoestimadodias() + "\n"
+					+" ID del solicitante = " + solicitud.getUsuario().getIdusuario() + "\n"
+				    +" Fecha de creación = " + solicitud.getFechaCreacion() + "\n"
+				    +" Estado= " + solicitud.getEstado() + "\n";
+			
+			
+		}
+		
+		
+		return reporte;
 	}
 	
 	
